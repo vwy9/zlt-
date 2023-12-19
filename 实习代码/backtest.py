964de01f63acad1e07d034ff3414ae38f3c46ef1
Vincent Xavier <100179229+vwy9@users.noreply.github.com>
2023-12-19 14:12:01 +0800

    Update backtest.py

--- 实习代码/backtest.py
+++ 实习代码/backtest.py
@@ -141,8 +141,6 @@ class IndexBacktest:
                                         - portfolio_return['stamp_tax']) / self.benchmark
         portfolio_return['r_return'].fillna(0, inplace=True)
 
-        print('cishi', self.benchmark)
-
         portfolio_return['rm_return'] = portfolio_rm['rm_sum'].diff() / self.benchmark
         portfolio_return['rm_return'].fillna(0, inplace=True)
 
@@ -271,6 +269,8 @@ class IndexBacktest:
         # 渲染图表到文件
         grid_chart.render(f"{date}_combined_chart.html")
 
+        return y_data_1, y_data_2, self.signals
+
     def run_backtest(self):
         if self.metrics is None:
             self.calculate_performance_metrics()
@@ -304,7 +304,7 @@ def calculate_metrics(combined_df, trading_date):
         'annualized_volatility_benchmark': annualized_volatility_benchmark,
         'sharpe_ratio': sharpe_ratio,
         'annualized_return_strategy': annualized_return_strategy,
-        'sortino_ratio': sortino_ratio
+        'sortino_ratio': sortino_ratio,
     }
 
     # 初始化一个新的 Series 来存储计算的差值，第一列设为 None 或 pd.NA
@@ -325,18 +325,18 @@ def calculate_metrics(combined_df, trading_date):
         # 如果不是第一列，则执行计算
         if previous_col is not None:
             r_overnight[current_col] = ((combined_df.at['r_start', current_col] - combined_df.at[
-                'r_end', previous_col]) / 2) / combined_df.at['r_start', initial_col]
+                'r_end', previous_col]) / 2) / combined_df.at['rm_start', initial_col]
             r_inday[current_col] = (combined_df.at['r_end', current_col] - combined_df.at['r_start', current_col]) \
-                / combined_df.at['r_start', initial_col]
+                / combined_df.at['rm_start', initial_col]
             r_day[current_col] = (combined_df.at['r_end', current_col] - combined_df.at['r_end', previous_col]) \
-                / combined_df.at['r_start', initial_col]
+                / combined_df.at['rm_start', initial_col]
 
             rm_overnight[current_col] = ((combined_df.at['rm_start', current_col] - combined_df.at[
-                'rm_end', previous_col]) / 2) / combined_df.at['r_start', initial_col]
+                'rm_end', previous_col]) / 2) / combined_df.at['rm_start', initial_col]
             rm_inday[current_col] = (combined_df.at['rm_end', current_col] - combined_df.at['rm_start', current_col]) \
-                / combined_df.at['r_start', initial_col]
+                / combined_df.at['rm_start', initial_col]
             rm_day[current_col] = (combined_df.at['rm_end', current_col] - combined_df.at['rm_end', previous_col]) \
-                / combined_df.at['r_start', initial_col]
+                / combined_df.at['rm_start', initial_col]
         else:
             # 如果是第一列，则赋值为空值
             r_overnight[current_col] = np.nan
@@ -357,7 +357,182 @@ def calculate_metrics(combined_df, trading_date):
     print(combined_df)
     print(metrics)
 
-    # 将计算出的差值作为新行添加到 DataFrame
+    return combined_df, metrics
+
+
+def calculate_max_drawdown_and_duration(net_values):
+    # 转换净值为最大回撤计算所需的累计最高净值
+    peak = net_values[0]
+    max_drawdown = 0
+    max_drawdown_duration = 0
+    duration_since_peak = 0
+
+    for value in net_values:
+        # 如果当前净值高于之前的峰值，则更新峰值
+        if value > peak:
+            peak = value
+            # 由于达到新高，持续时间重置
+            duration_since_peak = 0
+        else:
+            # 计算当前的回撤
+            drawdown = (peak - value) / peak
+            duration_since_peak += 1
+            # 如果当前回撤大于之前记录的最大回撤，则更新最大回撤和持续时间
+            if drawdown > max_drawdown:
+                max_drawdown = drawdown
+                max_drawdown_duration = duration_since_peak
+
+    return max_drawdown, max_drawdown_duration
+
+
+def mdd_and_alldayplt(df1, df2, df3, df4, metrics):
+    overnight_index1 = df2.index.get_loc('r_overnight')
+    for i in range(1, len(df1.columns)):
+        # 获取 df2 中相应列的 'r_overnight' 行的值
+        multiplier1 = df2.iloc[overnight_index1, i] + 1
+        # 确保 multiplier 是一个数值，如果不是，可以在这里添加额外的逻辑处理
+        if pd.notna(multiplier1) and isinstance(multiplier1, (int, float)):
+            # 更新 df1 中的值
+            df1.iloc[0, i] = df1.iloc[-1, i - 1] * multiplier1
+
+    overnight_index2 = df2.index.get_loc('rm_overnight')
+    for i in range(1, len(df3.columns)):
+        # 获取 df2 中相应列的 'r_overnight' 行的值
+        multiplier2 = df2.iloc[overnight_index2, i] + 1
+        # 确保 multiplier 是一个数值，如果不是，可以在这里添加额外的逻辑处理
+        if pd.notna(multiplier2) and isinstance(multiplier2, (int, float)):
+            # 更新 df1 中的值
+            df3.iloc[0, i] = df3.iloc[-1, i - 1] * multiplier2
+
+    df1_list = df1.T.values.flatten().tolist()
+    r_mdd, r_mdd_time = calculate_max_drawdown_and_duration(df1_list)
+    print("策略最大回撤为", r_mdd)
+    print("最大回撤周期为", r_mdd_time*3, "s")
+
+    df3_list = df3.T.values.flatten().tolist()
+    rm_mdd, rm_mdd_time = calculate_max_drawdown_and_duration(df3_list)
+    print("基准策略最大回撤为", rm_mdd)
+    print("最大回撤周期为", rm_mdd_time * 3, "s")
+
+    metrics['Strategy_MDD'] = r_mdd
+    metrics['Benchmark_MDD'] = rm_mdd
+    metrics['Strategy_MDD_Time'] = str(r_mdd_time*3) + 's'
+    metrics['Benchmark_MDD_Time'] = str(rm_mdd_time * 3) + 's'
+
+    signals_list = df4.T.values.flatten().tolist()
+
+    # # 确定Y轴的范围
+    y_line_min = round((min(min(df1_list), min(df3_list)) - 0.002), 3)
+    y_line_max = round((max(max(df1_list), max(df3_list)) + 0.002), 3)
+    y_bar_min = 0
+    y_bar_max = 1
+    #
+    datazoom_opts = [
+        opts.DataZoomOpts(is_show=True, xaxis_index=[0, 1]),
+        opts.DataZoomOpts(type_="inside", xaxis_index=[0, 1])
+    ]
+    x_labels = [str(i % 4802) for i in range(len(df1_list))]
+    # 创建折线图实例
+    line = Line()
+    # 添加x轴和y轴数据
+    line.add_xaxis(list(range(len(df1_list))))
+    line.add_yaxis(
+        series_name="策略累计回报",
+        y_axis=df1_list,
+        is_smooth=True,
+        linestyle_opts=opts.LineStyleOpts(width=2),
+        label_opts=opts.LabelOpts(is_show=False),
+    )
+    line.add_yaxis(
+        series_name="基准累计回报",
+        y_axis=df3_list,
+        is_smooth=True,
+        linestyle_opts=opts.LineStyleOpts(width=2),
+        label_opts=opts.LabelOpts(is_show=False),
+    )
+
+    # 创建柱状图实例
+    bar = Bar()
+    # 添加x轴和y轴数据
+    bar.add_xaxis(list(range(len(signals_list))))
+    bar.add_yaxis(
+        series_name="信号",
+        y_axis=signals_list,
+        label_opts=opts.LabelOpts(is_show=False)  # 设置不显示标签
+    )
+
+    # 设置折线图的全局选项
+    line.set_global_opts(
+        title_opts=opts.TitleOpts(title="累计回报对比"),
+        tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
+        legend_opts=opts.LegendOpts(pos_left="right"),
+        datazoom_opts=datazoom_opts,
+        yaxis_opts=opts.AxisOpts(type_="value", min_=y_line_min, max_=y_line_max),
+    )
+
+    # 设置柱状图的全局选项，注意不显示x轴，因为将会和折线图共用
+    bar.set_global_opts(
+        tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
+        legend_opts=opts.LegendOpts(pos_left="middle"),
+        xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(is_show=False)),
+        yaxis_opts=opts.AxisOpts(type_="value", min_=y_bar_min, max_=y_bar_max),
+        datazoom_opts=datazoom_opts
+    )
+
+    # 创建 Grid 对象
+    grid_chart = Grid()
+
+    grid_chart.add(
+        bar,
+        grid_opts=opts.GridOpts(
+            pos_left="5%",
+            pos_right="5%",
+            pos_top="75%",
+            height="20%"
+        ),
+    )
+
+    grid_chart.add(
+        line,
+        grid_opts=opts.GridOpts(
+            pos_left="5%",
+            pos_right="5%",
+            pos_top="10%",
+            height="60%"
+        ),
+    )
+
+    # 渲染图表到文件
+    grid_chart.render("123_combined_chart.html")
+
+    # 定义自定义文本的 HTML
+    custom_text_html = f"""
+        <div style="margin-right: 50px; flex: 1;">
+            <p>基准年化回报: {metrics['annualized_return_benchmark']}</p>
+            <p>基准年化波动率: {metrics['annualized_volatility_benchmark']}</p>
+            <p>夏普比率: {metrics['sharpe_ratio']:}</p>
+            <p>策略年化回报: {metrics['annualized_return_strategy']}</p>
+            <p>索提诺比率: {metrics['sortino_ratio']}</p>
+            <p>策略最大回撤为: {metrics['Strategy_MDD']}</p>
+            <p>基准策略最大回撤为: {metrics['Benchmark_MDD']}</p>
+            <p>策略最大回撤周期为: {metrics['Strategy_MDD_Time']}</p>
+            <p>基准策略最大回撤周期为: {metrics['Benchmark_MDD_Time']}</p>
+        </div>
+    """
+
+    # 假设您已经通过pyecharts生成了图表，并且已经渲染到 "chart.html" 文件中
+    chart_html_filename = "123_combined_chart.html"
+    # 读取图表的HTML内容
+    with open(chart_html_filename, "r", encoding="utf-8") as file:
+        chart_html_content = file.read()
+
+    chart_html_content = chart_html_content.replace("</body>", custom_text_html + "</body>")
+
+    # 将修改后的内容写回到文件中
+    with open("combined_chart.html", "w", encoding="utf-8") as file:
+        file.write(chart_html_content)
+
+    return df1, df3
 
 
 if __name__ == "__main__":
@@ -368,6 +543,9 @@ if __name__ == "__main__":
     end_date = int(input('请输入结束时间：'))
     trading_date = calculate_trade_days(start_date, end_date, conn)
     dfs = []
+    r_data = []
+    rm_data = []
+    signals_data = []
     benchmark_value = None
     initial_investment_weight = 0.4
     for date in trading_date:
@@ -377,7 +555,6 @@ if __name__ == "__main__":
         weight_data = pd.read_csv(f'./quote/{date}_detail/weight_matrix.csv')
         weight_data.drop(weight_data.columns[0], axis=1, inplace=True)
         weight_data = weight_data.add_prefix('weight_')
-        print('benchmark:', benchmark_value)
         backtest = IndexBacktest(weight_data, initial_investment_weight, benchmark_value)
 
         backtest.linear_full_signals()
@@ -391,7 +568,7 @@ if __name__ == "__main__":
         daily_metrics = backtest.calculate_performance_metrics()
         backtest.run_backtest()
         # 当日的所有行情数据自动生成html画图
-        backtest.plot_cumulative_returns(date)
+        datar, datarm, signals = backtest.plot_cumulative_returns(date)
         daily_dict = {'r_start': daily_portfolio['r_sum'][0], 'r_end': daily_portfolio['r_sum'].iloc[-1],
                       'r_min': daily_portfolio['r_sum'].min(), 'r_max': daily_portfolio['r_sum'].max(),
                       'rm_start': daily_portfolio['rm_sum'][0], 'rm_end': daily_portfolio['rm_sum'].iloc[-1],
@@ -403,13 +580,20 @@ if __name__ == "__main__":
                       'inday_alpha': daily_metrics['inday_alpha']}
         df = pd.DataFrame.from_dict(daily_dict, orient='index', columns=[f'{date}'])
         dfs.append(df)
+        r_data.append(datar)
+        rm_data.append(datarm)
+        signals_data.append(signals)
         # 将新的DataFrame拼接到已有的DataFrame上
     combined_df = pd.concat(dfs, axis=1)
-    print(combined_df)
+    r_data_df = pd.concat(r_data, axis=1)
+    rm_data_df = pd.concat(rm_data, axis=1)
+    signals_df = pd.concat(signals_data, axis=1)
 
-    print(combined_df)
+    dfc, metrics = calculate_metrics(combined_df, trading_date)
 
-    calculate_metrics(combined_df, trading_date)
+    df_r, df_rm = mdd_and_alldayplt(r_data_df, dfc, rm_data_df, signals_df, metrics)
+    print(df_r)
+    print(df_rm)
 
     conn.close()
     # 记录程序结束时间
